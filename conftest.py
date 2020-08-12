5674e1fd804344cd0557304eb532dbc721066323
MaxMarkes1013 <52193076+MaxMarkes1013@users.noreply.github.com>
2020-08-12 20:45:57 +0300

    Final editing amd adding of conftest file

--- conftest.py
+++ conftest.py
@@ -4,14 +4,14 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--language', action='store', default='ru', help='Chose language')
+    parser.addoption('--language', action='store', default=None, help='Choose language')
 
 
 @pytest.fixture(scope='function')
 def browser(request):
-    lang = request.config.getoption('language')
+    user_language = request.config.getoption('language')
     options = Options()
-    options.add_experimental_option('prefs', {'intl.accept_languages': lang})
+    options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
     print('\nstart Chrome browser...')
     browser = webdriver.Chrome(options=options)
     yield browser
